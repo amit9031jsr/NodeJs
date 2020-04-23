ad59dea2792e124208dfe2a0d46d3f7ca243d513
amit9031jsr <amit9031jsr@gmail.com>
2020-04-24 01:12:50 +0530

    Creating a Module

--- app.js
+++ app.js
@@ -9,3 +9,4 @@ clearTimeout()
 setInterval()
 
 clearInterval()
+
---
+++ logger.js
@@ -0,0 +1,9 @@
+//Creating a Module
+
+var url = 'http://mylogger.io/log';
+
+function log(msg) {
+    console.log(msg);
+}
+
+module.exports.log = log;
